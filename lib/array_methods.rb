c8f73649df6a8fedc33b23b8a53a36adf919945a
Seth Maisel <seth.maisel@gmail.com>
2019-09-30 22:08:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,8 @@
 def find_element_index(array, value_to_find)
-  def square_array(numbers)
-  counter = 0
-new_array = []
+  array = []
  
-while numbers [counter] do
-  new_array << numbers [counter] ** 2 
+while array [value_to_find] do
+  array.index 
   counter += 1
 end
   return new_array
