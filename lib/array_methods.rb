a9fc96f2721beaf5aa97077c97d0a7c98133e33a
Seth Maisel <seth.maisel@gmail.com>
2019-09-30 22:09:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   array = []
  
 while array [value_to_find] do
-  array.index (value_to_find) 
+  array.index(value_to_find) 
   value_to_find += 1
 end
   return array
